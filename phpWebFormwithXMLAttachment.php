d5fafffb6427271e1df9acfcb656b8fef6e4cdb3
Tower61 <Nik@Nik-Vales-MacBook-Pro.local>
2013-10-14 10:45:36 -0700

    Clean Up

--- phpWebFormwithXMLAttachment.php
+++ phpWebFormwithXMLAttachment.php
@@ -56,7 +56,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 		"Invalid email format");
 		
 	require_once('recaptchalib.php');
-	$privatekey = "<private_key>";
+	$privatekey = "<private_key>"; /* ENTER YOUR INFO HERE */
 	$resp = recaptcha_check_answer ($privatekey,
                                 	$_SERVER["REMOTE_ADDR"],
 									$_POST["recaptcha_challenge_field"],
@@ -146,11 +146,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 		
 		/* Email Detials */
 		
-			$mail_to   = "me@myemailaddress.com";
-			$from_mail = "webform@mywebsite.com";
-			$from_name = "PHP/XML Webform";
-			$reply_to  = "webform@mywebsite.com";
-			$subject   = "PHP/XML Webform";
+			$mail_to   = "me@myemailaddress.com"; /* ENTER YOUR INFO HERE */
+			$from_mail = "webform@mywebsite.com"; /* ENTER YOUR INFO HERE */
+			$from_name = "PHP/XML Webform";       /* ENTER YOUR INFO HERE */
+			$reply_to  = "webform@mywebsite.com"; /* ENTER YOUR INFO HERE */
+			$subject   = "PHP/XML Webform";       /* ENTER YOUR INFO HERE */
 		
 		/* The plain-text part of the message just lists all the collected data... */
 		
@@ -159,8 +159,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 		/* Attachment File */
 		
 			// No need to create an actual file, just use the content of the create XML document...
-			$content = $xmlDoc->saveXML();
-		
+			$content = chunk_split(base64_encode($xmlDoc->saveXML()));
+			
 		/* Create the email header */
 			
 			// Generate a boundary
@@ -183,17 +183,16 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 			$header .= "--".$boundary."\r\n";
 			
 			// Attachment
-			$header .= "Content-Type: application/xml; name=\"".$file_name."\"\r\n";
+			$header .= "Content-Type: application/xml; name=\""."attachment.xml"."\"\r\n";
 			$header .= "Content-Transfer-Encoding: base64\r\n";
-			$header .= "Content-Disposition: attachment; filename=\"".$file_name."\"\r\n\r\n";
+			$header .= "Content-Disposition: attachment; filename=\""."attachment.xml"."\"\r\n\r\n";
 			$header .= $content."\r\n";
 			$header .= "--".$boundary."--";
 			
 		error_log($mail_to."\r\n".$subject."\r\n".$message."\r\n".$header);
 		
 		/* Send email */
-		//if (mail($mail_to, $subject, $message, $header)) 
-		if(true) 
+		if (mail($mail_to, $subject, $message, $header)) 
 		{
 			$sent = true;
 		} else 
@@ -203,46 +202,182 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 	}
 }?>
 
+<style>
+#webform-wrapper
+{
+	-webkit-box-shadow: 2px 2px 10px rgba(50, 50, 50, 0.75);
+	-moz-box-shadow:    2px 2px 10px rgba(50, 50, 50, 0.75);
+	box-shadow:         2px 2px 10px rgba(50, 50, 50, 0.75);
 
-<!-- Display error / success message -->
-<?php
-	if($sent)
-	{
-		echo "Thanks ".$firstname." your form has been submitted.";
-	}
-	else
-	{
-		echo $sentErr;
-	}
-?>
+	width:450px;
+	margin:auto;
+	border-radius:8px;
+	border:1px solid #ccc;
+	padding:30px;
+	background-color:#eee;
+}
 
-<!-- Only display the form if it hasn't been sent yet -->
-<div style="<?php if($sent){echo 'display:none';}else{echo 'display:block';}?>">	
-	<form class="brochure_request" method="post" action=""> 
-		<p><span class="error">* required field.</span></p>   
-		<table width="100%" border="1">
-		<tr><td>First Name:</td><td><input type="text" name="firstname" value="<?php echo $firstname;?>"><br/><span class="error"><?php echo $firstnameErr;?></span></td><td class="error">* </td></tr>
-		<tr><td>Last Name:</td><td><input type="text" name="lastname" value="<?php echo $lastname;?>"><br/><span class="error"><?php echo $lastnameErr;?></span></td><td class="error">* </td></tr>
-		<tr><td>E-mail:</td><td><input type="text" name="email" value="<?php echo $email;?>"><br/><span class="error"><?php echo $emailErr;?></span></td><td class="error">*</span></td></tr>
-		<tr><td>Phone Number:</td><td><input type="text" name="phone" value="<?php echo $phone;?>"></td><td class="error"></td></tr>
-		<tr><td>Address Line 1:</td><td><input type="text" name="address1" value="<?php echo $address1;?>"></td><td class="error"></td></tr>
-		<tr><td>Address Line 2:</td><td><input type="text" name="address2" value="<?php echo $address2;?>"></td><td class="error"></td></tr>
-		<tr><td>City:</td><td><input type="text" name="addresscity" value="<?php echo $addresscity;?>"></td><td class="error"></td></tr>
-		<tr><td>State / Province:</td><td><input type="text" name="addressstate" value="<?php echo $addressstate;?>"></td><td class="error"></td></tr>
-		<tr><td>Zip / Postal Code:</td><td><input type="text" name="addresszip" value="<?php echo $addresszip;?>"></td><td class="error"></td></tr>
-		<tr><td>Country:</td><td><input type="text" name="addresscountry" value="<?php echo $addresscountry;?>"></td><td class="error"></td></tr>
-		<tr><td>Comment</td><td><textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea></td><td class="error"></td></tr>
+#webform-wrapper h3
+{
+	text-align: center;
+}
+
+#webform
+{
+	font-family: sans-serif;
+}
+
+#webform table
+{
+	border:none;
+	width:100%;
+}
+
+#webform table td
+{
+	border-bottom: 1px solid #efefef;
+}
+
+#webform input[type=text], #webform textarea
+{
+	width: 100%;
+	-webkit-appearance:none;
+	background-color: white;
+	border:thin solid #888;
+	border-radius: 3px;
+	font-size:11pt;
+	margin: 8px auto;
+	padding: 8px 8px;
+}
+
+#webform input[type=submit], #webform input[type=reset]
+{
+	-webkit-box-shadow: 2px 2px 10px rgba(50, 50, 50, 0.75);
+	-moz-box-shadow:    2px 2px 10px rgba(50, 50, 50, 0.75);
+	box-shadow:         2px 2px 10px rgba(50, 50, 50, 0.75);
+
+	background-image: none;
+	-webkit-appearance:none;
+	background-color: green;
+	border:1px solid #888;
+	border-radius: 5px;
+	color:white;
+	font-weight: bold;
+	margin: 0px 10px;
+	min-width: 100px;
+	padding:10px;
+	text-transform: uppercase;
+}
+#webform input[type=reset]
+{
+	background: red;
+}
+#webform input[type=reset]:hover
+{	
+	background: #6c1e1e;
+}
+#webform input[type=submit]:hover
+{
+	background-color: #133b13;
+}
+
+#webform .error, #webform .required
+{
+	color:red;
+	margin: 0px 10px 10px 10px;
+}
+</style>
+
+<div id="webform-wrapper">
+
+	<!-- Display error / success message -->
+	<h3>
+	<?php
+		if($sent)
+		{
+			echo "Thanks ".$firstname.", your form has been submitted.";
+		}
+		else
+		{
+			echo $sentErr;
+		}
+	?>	
+	</h3>
+	
+	<!-- Only display the form if it hasn't been sent yet -->
+	<form id="webform" method="post" action="#" style="<?php if($sent){echo 'display:none';}else{echo 'display:block';}?>;"> 
+		<h3>PHP Web Form with XML Attachment</h3>   
+		<table>
+			<tr>
+				<td><input type="text" name="firstname" placeholder="First Name" value="<?php echo $firstname;?>"><br/>
+					<p class="error"><?php echo $firstnameErr;?></p></td>
+				<td><p class="required">*</p></td>
+			</tr>
+			<tr>
+				<td><input type="text" name="lastname" placeholder="Last Name" value="<?php echo $lastname;?>"><br/>
+					<p class="error"><?php echo $lastnameErr;?></p></td>
+				<td><p class="required">*</p></td>
+			</tr>
+			<tr>
+				<td><input type="text" name="email" placeholder="E-mail" value="<?php echo $email;?>"><br/>
+					<p class="error"><?php echo $emailErr;?></p></td>
+				<td><p class="required">*</p></td>
+			</tr>
+			<tr>
+				<td><input type="text" name="phone" placeholder="555.555.5555" value="<?php echo $phone;?>"></td>
+				<td class="error"></td>
+			</tr>
+			<tr>
+				<td><input type="text" name="address1" placeholder="Address Line 1" value="<?php echo $address1;?>"></td>
+				<td class="error"></td>
+			</tr>
+			<tr>
+				<td><input type="text" name="address2" placeholder="Address Line 2" value="<?php echo $address2;?>"></td>
+				<td class="error"></td>
+			</tr>
+			<tr>
+				<td><input type="text" name="addresscity" placeholder="City" value="<?php echo $addresscity;?>"></td>
+				<td class="error"></td>
+			</tr>
+			<tr>
+				<td><input type="text" name="addressstate" placeholder="State / Province" value="<?php echo $addressstate;?>"></td>
+				<td class="error"></td>
+			</tr>
+			<tr>
+				<td><input type="text" name="addresszip" placeholder="Zip" value="<?php echo $addresszip;?>"></td>
+				<td class="error"></td>
+			</tr>
+			<tr>
+				<td><input type="text" name="addresscountry" placeholder="Country" value="<?php echo $addresscountry;?>"></td>
+				<td class="error"></td>
+			</tr>
+			<tr>
+				<td><textarea name="comment" placeholder="Tell us how much you like this form?" rows="5" cols="40"><?php echo $comment;?></textarea></td>
+				<td class="error"></td>
+			</tr>
 		<tr>
-		<td colspan=3 align="center">
-			<?php
-				require_once('recaptchalib.php');
-				$publickey = "<public_key>";
-				echo recaptcha_get_html($publickey);
-	        ?><br/>
-	        <span class="error"><?php echo $recaptcha_error;?></span>
-		</td>
+		<tr>
+			<td align="center" style="text-align:center">
+				<center>
+				<?php
+					require_once('recaptchalib.php');
+					$publickey = "<public_key>"; /* ENTER YOUR INFO HERE */
+					echo recaptcha_get_html($publickey);
+		        ?><br/>
+		        <p class="error"><?php echo $recaptcha_error;?></p>
+				</center>
+			</td>
+			<td></td>
 		</tr>
-		<tr><td colspan="2"><input type="submit" name="submit" value="submit"> </td><td></td></tr>
+		<tr>			
+			<td align="center" style="text-align:center">
+				<center>
+					<input type="reset" name="Reset" value="Reset">
+					<input type="submit" name="submit" value="submit">
+				</center>
+			</td>
+			<td></td>
+			</tr>
 		</table>	
 	</form>
 </div>
